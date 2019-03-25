a75684c283c0960765410ff15f591c70e4fd7f7a
TomasPetkevicius <48209088+TomasPetkevicius@users.noreply.github.com>
2019-03-25 21:07:55 +0200

    Add files via upload

---
+++ index01.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php01</title>
+</head>
+
+<body>
+   <?php 
+$salis='Lietuva';
+$miestas='Vilnius';
+$populiacija=574221;
+?>
+<ul> 
+    <li><?php echo $salis; ?></li>
+    <li><?php echo $miestas; ?></li>
+    <li><?php echo $populiacija; ?></li>
+</ul> 
+</body>
+</html>
+
